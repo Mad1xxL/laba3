e51f4bbedfad70e68f00a7a5f3bda263b67ceba2
Максим <mad1x@MacBook-Pro-Maksim-2.local>
2026-05-13 10:20:09 +0700

    added the code

--- 3zadanie.cpp
+++ 3zadanie.cpp
@@ -1,19 +1,39 @@
 #include <iostream>
 
+long long gcd (long long a, long long b){
+    return b == 0 ? a : gcd(b, a % b);
+}
+
 void matSum(int a, int b)    {
     if (b == 1) {
-        std::cout << "Infinity" << std::endl;
+        std::cout << "infinity" << std::endl;
         return;
     }
+
+    int num;
+    int den;
+
     if (a == 1) {
+        num = b;
+        den = (b-1) * (b-1);
+    }
+
+    else if (a == 2) {
+        num = b * (b+1);
+        den = (b-1) * (b-1) * (b-1);
+    }
+
+    else if (a == 3) {
+        num = b * (b * b + 4 * b + 1);
+        den = (b-1) * (b-1) * (b-1) * (b-1);
+    }
 
     }
-    if (a == 2) {
 
-    }
-    if (a == 3) {
-        
-    }
+    num /= nod;
+    den /= nod;
+
+    std::cout << num << "/" << den << std::endl;
 }
 
 int main()  {
@@ -25,7 +45,14 @@ int main()  {
     std::cout << "Введите значение b: ";
     std::cin >> b;
 
+    if (b == 1) {
+        std::cout << "infinity" << std::endl;
+        return 0;
+    }
 
+    
+
+    matSum(a,b);
     
     return 0;
 }

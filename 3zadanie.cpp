8e2d31f2d4f07ade7020504f48fe5c4aa6c2fba0
Максим <mad1x@MacBook-Pro-Maksim-2.local>
2026-05-13 07:37:15 +0700

    Create Created .cpp and then wrote the skeleton

---
+++ 3zadanie.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+void matSum(int a, int b)    {
+    if (b == 1) {
+        std::cout << "Infinity" << std::endl;
+        return;
+    }
+    if (a == 1) {
+
+    }
+    if (a == 2) {
+
+    }
+    if (a == 3) {
+        
+    }
+}
+
+int main()  {
+    int a;
+    std::cout << "Введите значение a: ";
+    std::cin >> a;
+
+    int b;
+    std::cout << "Введите значение b: ";
+    std::cin >> b;
+
+
+    
+    return 0;
+}
